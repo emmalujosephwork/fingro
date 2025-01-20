00ff5a1bfedfea06f391119967146968a163bae2
emmalujosephwork <emmalujosephwork@gmail.com>
2025-01-20 16:36:14 +1100

    added signup page

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const appController = require('../controllers/appController'); // Correct path to the controller
 
-
 // Home Route
 router.get('/', appController.homePage);
 
@@ -11,5 +10,11 @@ router.get('/grocery-plan', appController.groceryPlan);
 
 // Money Manager Route
 router.get('/money-manager', appController.moneyManager);
+
+// Login Route
+router.get('/login', appController.loginPage); // Add this route for login page
+
+// Signup Route (you can add this route if required for signup page)
+router.get('/signup', appController.signupPage);
 
 module.exports = router;
