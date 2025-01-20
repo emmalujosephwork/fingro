580cb20ecf8c0afee082c923d6a5f9ea6de51213
emmalujosephwork <emmalujosephwork@gmail.com>
2025-01-21 09:40:23 +1100

    db connection done

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const User = require('../models/user'); // Import the User model
 
 // Home Page
 exports.homePage = (req, res) => {
@@ -15,14 +15,43 @@ exports.moneyManager = (req, res) => {
     res.sendFile('money-manager.html', { root: './views' });
 };
 
-// Login Page (new route for login)
+// Login Page
 exports.loginPage = (req, res) => {
     res.sendFile('login.html', { root: './views' });
 };
 
-// Signup Page (optional, in case you need a signup route)
+// Signup Page
 exports.signupPage = (req, res) => {
     res.sendFile('signup.html', { root: './views' });
+};
+
+// Handle Sign Up form submission (POST request)
+exports.handleSignup = async(req, res) => {
+    const { name, email, password, mobile } = req.body;
+
+    // Check if all required fields are filled
+    if (!name || !email || !password || !mobile) {
+        return res.status(400).send('All fields are required');
+    }
+
+    try {
+        // Create new user
+        const newUser = new User({
+            name,
+            email,
+            password, // Password will be hashed automatically before saving
+            mobile
+        });
+
+        // Save the user to the database
+        await newUser.save();
+
+        // Redirect to login page after successful sign up
+        res.redirect('/login');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error saving user to database');
+    }
 };
 
 // About Page
