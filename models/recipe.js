5f4dfe98566f40bcc0e55a39c06f47443da02b4b
emmalujosephwork <emmalujosephwork@gmail.com>
2025-02-05 04:21:56 +1100

    display recipies

--- models/recipe.js
+++ models/recipe.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const recipeSchema = new mongoose.Schema({
     dishName: String,
     ingredients: { type: Map, of: String }, // Using Map to store ingredients as key-value pairs
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Add userId field
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
